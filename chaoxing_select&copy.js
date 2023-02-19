59807ce719f159e226d24f24356ed8af74fbae3e
Ding Shuai <1203823603@qq.com>
2023-02-19 23:14:06 +0800

    Rename to English

--- chaoxing_select&copy.js
+++ chaoxing_select&copy.js
@@ -2,7 +2,7 @@
 // @name         学习通选中复制
 // @namespace    dingshuai
 // @version      1.1
-// @description  学习通题目可以选中复制，便于搜索
+// @description  学习通题目可以选中复制，便于搜索题目
 // @author       dingshuai
 // @match        https://mooc1.chaoxing.com/exam-ans/*
 // @icon         https://www.google.com/s2/favicons?domain=chaoxing.com
